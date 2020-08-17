b994bf59882fee798865ac5920d3f55f16849715
busramerveaydn <32596414+busramerveaydn@users.noreply.github.com>
2020-08-17 15:59:44 +0300

    Added
    Bir sayının asal sayı olup olmadığını bulan program

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayiBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPrimeNumber(7))
+            {
+                Console.WriteLine("This is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number % 2 == 0)
+                {
+                    result = false;
+                    i = number;//döngüyü sonlandırmak için
+                }
+            }
+            return result;
+        }
+    }
+}
